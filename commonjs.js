40ab4ce53a0c93d17376941a9398cd89e5dd8f2e
Phara0h <jtwhissel@gmail.com>
2021-03-22 12:18:54 -0400

    Create commonjs.js

---
+++ commonjs.js
@@ -0,0 +1,14 @@
+module.exports = {
+    regex: new RegExp('{{([^}]+)}}', 'g'),
+    transform: function(template, objs) {
+      return template.replace(this.regex,(match, prop) =>{
+          return objs[prop];
+      });
+    },
+    compile: function(template) {
+      return (objs)=>{
+        return this.transform(template, objs);
+      }
+    },
+  };
+  
